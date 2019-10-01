6961325248c6d54a99f67f144a89dd7366c1ee37
Artyom <51087431+artyomchch@users.noreply.github.com>
2019-10-01 19:04:43 +0300

    task 6.4

--- app/src/main/java/com/example/secondlab/MainActivity.java
+++ app/src/main/java/com/example/secondlab/MainActivity.java
@@ -1,8 +1,19 @@
 package com.example.secondlab;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Gravity;
@@ -14,10 +25,18 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+
     private static final String TAG = "TestTask4.0";
-    Button  Activity;
+    private static final String CHANNEL_ID = "com.mirea.asd.notification.ANDROID";
+    private int IDENTIFICATE_MSG = 0;
+
+    Button Activity;
     Button Browser;
     Button ToastTask;
+    Button Noti;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,6 +50,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         ToastTask = findViewById(R.id.toast);
         ToastTask.setOnClickListener(this);
+
+        Noti = findViewById(R.id.noti);
+        Noti.setOnClickListener(this);
+
 
     }
 
@@ -52,15 +75,56 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 startActivity(sec);
                 break;
             case R.id.toast:
-                   Toast toast = Toast.makeText(getApplicationContext(),"Welcome to MIREA!", Toast.LENGTH_SHORT);
-                toast.setGravity(Gravity.BOTTOM, 0, 0);
-                LinearLayout toastContainer = (LinearLayout) toast.getView();
-                ImageView catImageView = new ImageView(getApplicationContext());
-                catImageView.setImageResource(R.drawable.ic_launcher_background);
-                toastContainer.addView(catImageView, 0);
-                toast.show();
+                   toast();
+            case R.id.noti:
+                notification(view);
+               break;
             default:
+
                 break;
         }
+    }
+
+
+    public void toast(){
+        Toast toast = Toast.makeText(getApplicationContext(),"Welcome to MIREA!", Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.BOTTOM, 0, 0);
+        LinearLayout toastContainer = (LinearLayout) toast.getView();
+        ImageView catImageView = new ImageView(getApplicationContext());
+        catImageView.setImageResource(R.drawable.ic_launcher_background);
+        toastContainer.addView(catImageView, 0);
+        toast.show();
+    }
+
+    public void notification(View view){
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+// Create PendingIntent
+        Intent resultIntent = new Intent(this, MainActivity.class);
+        PendingIntent resultPendingIntent = PendingIntent.getActivity(this, 0, resultIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel(CHANNEL_ID,
+                    "My Notifications", NotificationManager.IMPORTANCE_DEFAULT);
+// Configure the notification channel.
+            notificationChannel.setDescription("Channel description");
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.setVibrationPattern(new long[]{0, 1000, 500, 1000});
+            notificationChannel.enableVibration(true);
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
+// Create Notification
+        NotificationCompat.Builder builder =
+                new NotificationCompat.Builder(this, CHANNEL_ID)
+                        .setSmallIcon(R.mipmap.ic_launcher)
+                        .setContentTitle("Title")
+                        .setContentText("Notification text for MIREA")
+                        .setWhen(System.currentTimeMillis())
+                        .setProgress(100, 50, false)
+                        .setContentIntent(resultPendingIntent);
+        Notification notification = builder.build();
+// Show Notification
+        notificationManager.notify(IDENTIFICATE_MSG++, notification);
     }
 }
